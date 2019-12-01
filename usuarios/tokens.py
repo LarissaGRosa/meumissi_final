3c3644b9095b413facbc78728e0738de311f7750
MeuMissi <meumissi@gmail.com>
2019-12-01 01:54:34 -0300

    Missi commit 2

--- usuarios/tokens.py
+++ usuarios/tokens.py
@@ -7,7 +7,12 @@ class UserTokenGenerator(PasswordResetTokenGenerator):
         user_id = six.text_type(user.pk)
         ts = six.text_type(timestamp)
         is_active = six.text_type(user.is_active)
-        return f"{user_id}{ts}{is_active}"
+        context = {
+            'user_id': user_id,
+            'ts': ts,
+            'is_active': is_active,
+        }
+        return context
 
 
 user_tokenizer = UserTokenGenerator()
